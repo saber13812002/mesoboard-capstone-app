bcd290d99f5fbd01afed1497eb79009807971a0b
keviinramiirez <kevin.ramirez3@upr.edu>
2021-10-20 23:36:35 -0400

    initial commit

--- mesoboard-api/index.js
+++ mesoboard-api/index.js
@@ -2,13 +2,13 @@
 const path = require('path');
 const express = require('express');
 
-const cors = require('cors');
+// const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
