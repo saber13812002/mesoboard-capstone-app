31d5a63730eae59a3b247893dcec8a07c4764715
keviinramiirez <kevin.ramirez3@upr.edu>
2021-10-25 22:05:05 -0400

    implemented schedule turns table with button

--- mesoboard-app/src/content/index.js
+++ mesoboard-app/src/content/index.js
@@ -5,5 +5,18 @@ import CheckManager from "./manager/CheckManager/CheckManager";
 import ProfileManager from "./manager/ProfileManager/ProfileManager";
 import RequestManager from "./manager/RequestManager/RequestManager";
 import MemoManager from "./manager/MemoManager/MemoManager";
+import ScheduleTable from './manager/ScheduleManager/ScheduleTable'
+import ScheduleTurnsTable from './manager/ScheduleManager/ScheduleTurnsTable'
+import ScheduleHoursBox from './shared/ScheduleShared/ScheduleHoursBox'
 
-export { HomeManager, ScheduleManager, CheckManager,ProfileManager,RequestManager,MemoManager }
+export {
+  HomeManager,
+  ScheduleManager,
+  CheckManager,
+  ProfileManager,
+  RequestManager,
+  MemoManager,
+  ScheduleTable,
+  ScheduleTurnsTable,
+  ScheduleHoursBox
+}
---
+++ mesoboard-app/src/content/manager/ScheduleManager/ScheduleTurnsTable.js
@@ -0,0 +1,42 @@
+import './ScheduleTurnsTable.css'
+import { Table } from 'react-bootstrap'
+import { MButton, ICON_OPTIONS } from '../../../components'
+
+const ScheduleTurnsTable = ({ turns, onAddNewTurn }) => {
+  return (
+    <div className='scheduleTurns'>
+      <h5>TURNOS</h5>
+      <Table responsive size="sm" className='scheduleTurns__table'>
+        <thead>
+          <tr style={{ fontSize: '14px' }}>
+            <td>ID</td>
+            <td>Entrada</td>
+            <td>Salidas</td>
+            <td>Almuerzo</td>
+          </tr>
+        </thead>
+        <tbody>
+          {turns.map(({ id, start, end, lunch }) =>
+            <tr style={{ fontWeight: '500' }}>
+              <td><strong>{id}</strong></td>
+              <td>{start}</td>
+              <td>{end}</td>
+              <td>{lunch}</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+      <MButton
+        onClick={onAddNewTurn}
+        icon={ICON_OPTIONS.plus}
+        text='Nuevo Turno'
+        variant='primary'
+        size='sm'
+        className='ml-2 pt-2'
+        style={{ marginTop: '-48px' }}
+      />
+    </div>
+  )
+}
+
+export default ScheduleTurnsTable
