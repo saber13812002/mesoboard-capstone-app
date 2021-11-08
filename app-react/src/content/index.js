55e44765319c203442ad4a0fada15fc4ebe4b234
keviinramiirez <kevin.ramirez3@upr.edu>
2021-11-07 22:44:30 -0400

    content body management by url's state

---
+++ app-react/src/content/index.js
@@ -0,0 +1,22 @@
+// manager
+import HomeManager from "./manager/HomeManager/HomeManager";
+import ScheduleManager from "./manager/ScheduleManager/ScheduleManager";
+import CheckManager from "./manager/CheckManager/CheckManager";
+import ProfileManager from "./manager/ProfileManager/ProfileManager";
+import RequestManager from "./manager/RequestManager/RequestManager";
+import MemoManager from "./manager/MemoManager/MemoManager";
+import ScheduleTable from './manager/ScheduleManager/ScheduleTable'
+import ScheduleTurnsTable from './manager/ScheduleManager/ScheduleTurnsTable'
+import ScheduleHoursBox from './shared/ScheduleShared/ScheduleHoursBox'
+
+export {
+  HomeManager,
+  ScheduleManager,
+  CheckManager,
+  ProfileManager,
+  RequestManager,
+  MemoManager,
+  ScheduleTable,
+  ScheduleTurnsTable,
+  ScheduleHoursBox
+}
---
+++ app-react/src/layout/ContentView/ContentView.js
@@ -0,0 +1,46 @@
+import './ContentView.css'
+import { urlSlug } from '../../services/url'
+import { useRouteMatch } from 'react-router-dom'
+import { ContentHeader } from '../../components'
+import {
+  HomeManager,
+  ScheduleManager,
+  CheckManager,
+  ProfileManager,
+  MemoManager,
+  RequestManager
+} from '../../content'
+
+/** returns the component to be viewed */
+const handleView = view => {
+  switch (view) {
+    case 'home':
+      return <HomeManager />
+    case 'schedule':
+      return <ScheduleManager />
+    case 'checks':
+      return <CheckManager />
+    case 'profiles':
+      return <ProfileManager />
+    case 'request':
+      return <RequestManager />
+    default:
+      return <MemoManager />
+  }
+}
+
+const ContentView = () => {
+  let { url } = useRouteMatch();
+  // console.log('url', urlSlug(url))
+
+  return (
+    <div className='body'>
+      <ContentHeader view={urlSlug(url)} />
+      <div className='body__content'>
+        {handleView(urlSlug(url))}
+      </div>
+    </div>
+  )
+}
+
+export default ContentView
