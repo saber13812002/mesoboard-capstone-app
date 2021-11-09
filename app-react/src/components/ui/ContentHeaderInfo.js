de9aaa3e9e6f91673fc15802e7652366090cb99e
keviinramiirez <kevin.ramirez3@upr.edu>
2021-11-09 14:21:28 -0400

    only display right info for intended component

--- app-react/src/components/ui/ContentHeaderInfo.js
+++ app-react/src/components/ui/ContentHeaderInfo.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import { TotalServicioProduccion } from '..'
 
 const ContentHeaderInfo = ({ view }) => {
-  // const [infoTo]
-  if (view === 'schedule') {
-    return <TotalServicioProduccion />
-  }
+  const [displayTotalAmountInfo, setDisplayTotalAmountInfo] = useState(false)
+  const [displayTotalProductionInfo, setDisplayTotalProductionInfo] = useState(false)
+
+  useEffect(() => {
+    if (view === 'schedule') {
+      setDisplayTotalProductionInfo(true)
+      setDisplayTotalAmountInfo(false)
+    }
+    else if (view === 'profiles') {
+      setDisplayTotalAmountInfo(true)
+      setDisplayTotalProductionInfo(false)
+    }
+    else {
+      setDisplayTotalAmountInfo(false)
+      setDisplayTotalProductionInfo(false)
+    }
+  }, [view])
 
   return (
-    <div>
-      display total amount component here
-    </div>
+    <>
+      {displayTotalAmountInfo && <p>display total amount component here</p>}
+      {displayTotalProductionInfo && <TotalServicioProduccion />}
+    </>
   )
 }
 
