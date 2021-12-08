061efe804c9dc2e6f5b746acc827d377547e61e2
keviinramiirez <kevin.ramirez3@upr.edu>
2021-12-07 22:53:09 -0400

    .

--- config/express.js
+++ config/express.js
@@ -12,45 +12,45 @@ module.exports = function () {
   const app = express();
   const server = http.createServer(app);
 
-  app.use(methodOverride());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(cors());
+  // app.use(methodOverride());
+  // app.use(express.json());
+  // app.use(express.urlencoded({ extended: true }));
+  // app.use(cors());
 
-  // const allowedOrigins = ['http://localhost:3000', 'https://mesoboard-capstone-app.herokuapp.com', 'http://mesoboard-capstone-app.herokuapp.com', 'http://mesoboard-capstone-app.herokuapp.com/3001'];
-  // const corsOptions = {
-  //   origin: function (origin, callback) {
-  //     // allow requests with no origin 
-  //     // (like mobile apps or curl requests)
-  //     if (!origin) return callback(null, true);
-  //     if (allowedOrigins.indexOf(origin) === -1) {
-  //       const msg = 'The CORS policy for this site does not ' +
-  //         'allow access from the specified Origin.';
-  //       return callback(new Error(msg), false);
-  //     }
-  //     return callback(null, true);
-  //   },
-  //   methods: 'POST, GET, OPTIONS, PUT, DELETE',
-  //   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, token, api_key'
-  // };
-  // app.use(cors(corsOptions));
-  ////// app.use(fileUpload());
+  // // const allowedOrigins = ['http://localhost:3000', 'https://mesoboard-capstone-app.herokuapp.com', 'http://mesoboard-capstone-app.herokuapp.com', 'http://mesoboard-capstone-app.herokuapp.com/3001'];
+  // // const corsOptions = {
+  // //   origin: function (origin, callback) {
+  // //     // allow requests with no origin 
+  // //     // (like mobile apps or curl requests)
+  // //     if (!origin) return callback(null, true);
+  // //     if (allowedOrigins.indexOf(origin) === -1) {
+  // //       const msg = 'The CORS policy for this site does not ' +
+  // //         'allow access from the specified Origin.';
+  // //       return callback(new Error(msg), false);
+  // //     }
+  // //     return callback(null, true);
+  // //   },
+  // //   methods: 'POST, GET, OPTIONS, PUT, DELETE',
+  // //   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, token, api_key'
+  // // };
+  // // app.use(cors(corsOptions));
+  // ////// app.use(fileUpload());
 
-  /** Routes that contain '/protected' in their path string will first
-   *  verify the user's authorization by a Bearer Token and assign the
-   *  jwt payload to the request object if verification was successful.
-   */
-  app.use('/protected', require('../controllers/security').verifyJWT);
+  // /** Routes that contain '/protected' in their path string will first
+  //  *  verify the user's authorization by a Bearer Token and assign the
+  //  *  jwt payload to the request object if verification was successful.
+  //  */
+  // app.use('/protected', require('../controllers/security').verifyJWT);
 
-  /***************************/
-  /* REQUIRE THE ROUTES HERE */
-  /***************************/
-  require('../routes/authentication-routes')(app);
-  require('../routes/mailer-routes')(app);
-  require('../routes/permission-routes')(app);
-  require('../routes/schedule-routes')(app);
-  require('../routes/profiles-routes')(app);
-  require('../routes/notification-routes')(app);
+  // /***************************/
+  // /* REQUIRE THE ROUTES HERE */
+  // /***************************/
+  // require('../routes/authentication-routes')(app);
+  // require('../routes/mailer-routes')(app);
+  // require('../routes/permission-routes')(app);
+  // require('../routes/schedule-routes')(app);
+  // require('../routes/profiles-routes')(app);
+  // require('../routes/notification-routes')(app);
 
   // Have Node serve the files for our built React app
   app.use(express.static(path.resolve(__dirname, '../app-react/build')));
