0a9b6c72a813d8b71b194fdcb18c8e618e534979
keviinramiirez <kevin.ramirez3@upr.edu>
2021-12-07 23:18:13 -0400

    works with only mailer-route, I assume that it breaks with routes that use the db

--- config/express.js
+++ config/express.js
@@ -12,29 +12,29 @@ module.exports = function () {
   const app = express();
   const server = http.createServer(app);
 
-  // app.use(methodOverride());
-  // app.use(express.json());
-  // app.use(express.urlencoded({ extended: true }));
-  // app.use(cors());
+  app.use(methodOverride());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
 
-  // // const allowedOrigins = ['http://localhost:3000', 'https://mesoboard-capstone-app.herokuapp.com', 'http://mesoboard-capstone-app.herokuapp.com', 'http://mesoboard-capstone-app.herokuapp.com/3001'];
-  // // const corsOptions = {
-  // //   origin: function (origin, callback) {
-  // //     // allow requests with no origin 
-  // //     // (like mobile apps or curl requests)
-  // //     if (!origin) return callback(null, true);
-  // //     if (allowedOrigins.indexOf(origin) === -1) {
-  // //       const msg = 'The CORS policy for this site does not ' +
-  // //         'allow access from the specified Origin.';
-  // //       return callback(new Error(msg), false);
-  // //     }
-  // //     return callback(null, true);
-  // //   },
-  // //   methods: 'POST, GET, OPTIONS, PUT, DELETE',
-  // //   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, token, api_key'
-  // // };
-  // // app.use(cors(corsOptions));
-  // ////// app.use(fileUpload());
+  // const allowedOrigins = ['http://localhost:3000', 'https://mesoboard-capstone-app.herokuapp.com', 'http://mesoboard-capstone-app.herokuapp.com', 'http://mesoboard-capstone-app.herokuapp.com/3001'];
+  // const corsOptions = {
+  //   origin: function (origin, callback) {
+  //     // allow requests with no origin 
+  //     // (like mobile apps or curl requests)
+  //     if (!origin) return callback(null, true);
+  //     if (allowedOrigins.indexOf(origin) === -1) {
+  //       const msg = 'The CORS policy for this site does not ' +
+  //         'allow access from the specified Origin.';
+  //       return callback(new Error(msg), false);
+  //     }
+  //     return callback(null, true);
+  //   },
+  //   methods: 'POST, GET, OPTIONS, PUT, DELETE',
+  //   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, token, api_key'
+  // };
+  // app.use(cors(corsOptions));
+  ////// app.use(fileUpload());
 
   /** Routes that contain '/protected' in their path string will first
    *  verify the user's authorization by a Bearer Token and assign the
@@ -45,7 +45,7 @@ module.exports = function () {
   // /***************************/
   // /* REQUIRE THE ROUTES HERE */
   // /***************************/
-  require('../routes/authentication-routes')(app);
+  // require('../routes/authentication-routes')(app);
   require('../routes/mailer-routes')(app);
   // require('../routes/permission-routes')(app);
   // require('../routes/schedule-routes')(app);
