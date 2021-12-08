b9ea2fe1b7fbb8780f73e0d08364c1071a439d2a
keviinramiirez <kevin.ramirez3@upr.edu>
2021-12-08 00:12:59 -0400

    db Pool in production

--- config/postgres.js
+++ config/postgres.js
@@ -1,5 +1,40 @@
+// // Load the module dependencies
+// const config = require('./config');
+
+// /* http://bluebirdjs.com/docs/getting-started.html
+//   replace ES6 promises
+// */
+// const promise = require('bluebird');
+
+// // overrides pg-promise default promise library - ES6 Promises - with Bluebird, supuestamente es mejor que ES6 promises.
+// const options = {
+//   // Initialization Options
+//   promiseLib: promise
+// };
+
+// //  overrode pg-promise’s default promise library - ES6 Promises - 
+// // with Bluebird by setting the promiseLib property in the options object.
+// const pgp = require('pg-promise')(options);
+
+// // console.log('config', config)
+// // Use pgp to connect to postgres
+// const db = pgp(config.connectionString);
+
+// // Define the postgres configuration method
+// module.exports = function () {
+//   //return connection instance
+//   return db;
+// };
+
+
+
+
+
 // Load the module dependencies
 const config = require('./config');
+
+const Pool = require('pg').Pool;
+require('dotenv').config()
 
 /* http://bluebirdjs.com/docs/getting-started.html
   replace ES6 promises
@@ -18,7 +53,16 @@ const pgp = require('pg-promise')(options);
 
 // console.log('config', config)
 // Use pgp to connect to postgres
-const db = pgp(config.connectionString);
+let db;
+
+if (process.env.NODE_ENV === 'production') {
+  const profConfig = { connectionString: process.env.DATABASE_URL };
+  const pool = new Pool(profConfig);
+  db = pool;
+}
+else {
+  db = pgp(config.connectionString);
+}
 
 // Define the postgres configuration method
 module.exports = function () {
